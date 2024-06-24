8b4556f4468db202f373151a32df95b566632f69
effectmoe <info@effect.moe>
2024-06-24 14:45:22 +0900

    1

--- dist/client_app.js
+++ dist/client_app.js
@@ -1,7 +1,7 @@
-import bootstrap from 'bootstrap/dist/js/bootstrap.js';
-import 'bootstrap/dist/css/bootstrap.css';
-import $ from 'jquery/dist/jquery.min.js';
-import MeboryClient from 'local_lib/MeboryClient.js';
+import bootstrap from './bootstrap/dist/js/bootstrap.js';
+import './bootstrap/dist/css/bootstrap.css';
+import $ from './jquery/dist/jquery.min.js';
+import MeboryClient from './local_lib/MeboryClient.js';
 
 let meboryClient = new MeboryClient('C_34e7fe65-018f-1000-a264-c7691938a0f2', 'mebory-login', 'https://login.mebory.com');
 
